75b6144bc5693352e997bfb22d8cbc0797ccf729
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-04-10 12:20:02 +0000

    print updates

--- testProject/Printing.py
+++ testProject/Printing.py
@@ -1,12 +1,5 @@
-for j in range(4):
-    print("# ", end="")
+for i in range(4):
+    for j in range(4):
+        print("# ", end="")
 
-print()
-
-for j in range(4):
-    print("# ", end="")
-
-print()
-
-for j in range(4):
-    print("# ", end="")
+    print()
