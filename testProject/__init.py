4f6cb725b16fdeea7ff44adfc620eb77dd7aef08
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-04-26 11:33:11 +0000

    init updates

---
+++ testProject/__init.py
@@ -0,0 +1,16 @@
+
+
+class Computer:
+
+    def config(self):
+        print("Macbook pro")
+
+
+com1 = Computer()
+com2 = Computer()
+
+com1.config()
+com2.config()
+
+Computer.config()
+Computer.config()
