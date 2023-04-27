5865c0d6f26db1acc85a1d297a1d9461f439b87f
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-04-27 11:06:24 +0000

    __init updates

--- testProject/__init.py
+++ testProject/__init.py
@@ -2,17 +2,19 @@
 
 class Computer:
 
-    def __init__(self,cpu,ram):
-        self.cpu = cpu
-        self.ram = ram
+    
 
-    def config(self):
-        print("Config is ", self.cpu, self.ram)
+#     def __init__(self,cpu,ram):
+#         self.cpu = cpu
+#         self.ram = ram
+
+#     def config(self):
+#         print("Config is ", self.cpu, self.ram)
 
 
-com1 = Computer("Macbook Pro", 16)
-com2 = Computer("Dell Vostro", 8)
+# com1 = Computer("Macbook Pro", 16)
+# com2 = Computer("Dell Vostro", 8)
 
-com1.config()
-com2.config()
+# com1.config()
+# com2.config()
 
