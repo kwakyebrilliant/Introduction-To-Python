d9c96d9f89803f8a4b6d7c3662e92a6c992bb210
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-01 20:12:44 +0000

    updates

--- testProject/Operator.py
+++ testProject/Operator.py
@@ -8,9 +8,5 @@ class Student:
         self.m2 = m2
 
 
-    def __add__(self, other):
-        m1 = self.m1 + other.m1
-        m2 = self.m2 + other.m2
-        s3 = Student(m1, m2)
 
-        return s3
+s1 = Student(58,69)
