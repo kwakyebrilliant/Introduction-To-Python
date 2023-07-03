e39dbd209d352e29d60b9d419ab6e253661bd7c7
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-07-03 11:50:11 +0000

    iterator updates

--- testProject/Itereator.py
+++ testProject/Itereator.py
@@ -5,3 +5,20 @@ class TopTen:
 
     def __iter__(self):
         return self
+    
+    def __next__(self):
+
+        if self.num <= 10:
+            val = self.num
+            self.num += 1
+
+            return val
+        
+        else:
+            raise StopIteration
+    
+
+values = TopTen()
+
+for i in values:
+    print(i)
